dec237f7a64cf7d13ce738f73c9f4d53e4601c5a
Sayed Farhad Nabizada <nzfarhad@gmail.com>
2023-07-28 20:04:38 +0500

    Optimized the code

--- scrape_article_titles.R
+++ scrape_article_titles.R
@@ -20,7 +20,7 @@ links_data <- data.frame(row.names = 1)
 
 # Iterate over all pages in Tolo News website and get the article title, date, and links
 total_pages <- 2749
-for (i in 1:total_pages - 1) {
+for (i in 1:total_pages) {
   #English link
   url <- paste0('https://tolonews.com/afghanistan/all?page=',i)
   #Dari link
@@ -35,30 +35,35 @@ for (i in 1:total_pages - 1) {
   ar_title2 <- html_nodes(webpage,'.hidden-onmobile')
   get_title2 <- html_text(ar_title2, trim = T)
   get_title2 <- get_title2[4:13]
-  # page_id_all <- append(page_id_all, page_id, after = length(page_id_all))
-  # get_date <- html_text(ar_date)
-  # all_tittles <- append(all_tittles, get_title, after = length(all_tittles))
+ 
   all_tittles_df <- data.frame(get_title, get_title2, page_id)
   titles_df <- rbind(titles_df,all_tittles_df )
   
   ## dates
-  date1 <- xml_children(ar_date)
-  date2 <- as_list(date1)
-  date_list <- lapply(date2, attributes)
-  dates_d_frame <- do.call(rbind, date_list)
-  dates_frame <- as.data.frame(dates_d_frame)
-  dates_frame$page_id <- page_id
+  dates_frame <- xml_children(ar_date) %>% 
+    as_list() %>% 
+    lapply(attributes) %>% 
+    do.call(rbind, .) %>% 
+    as.data.frame() %>% 
+    mutate(
+      page_id = page_id
+    )
+  
   dates_data <- rbind(dates_data, dates_frame)
   
   
   ## links
-  link1 <- xml_children(ar_title)
-  link2 <- as_list(link1)
-  link3 <- lapply(link2, attributes)
-  link_list <- do.call(rbind, link3)
-  links <- as.data.frame(link_list)
-  links$page_id <- page_id
+  links <- xml_children(ar_title) %>% 
+    as_list() %>% 
+    lapply(attributes) %>% 
+    do.call(rbind, .) %>% 
+    as.data.frame() %>% 
+    mutate(
+      page_id = page_id
+    )
+    
   links_data <- rbind(links_data, links)
+  
   
   cat("\014")  
   print(paste0("Page ",i, " of ", total_pages, " Scraped!"))
@@ -67,13 +72,17 @@ for (i in 1:total_pages - 1) {
 }
 
 # merge data
-merged_new_data <- cbind(titles_df, dates_data, links_data)
+merged_data <- cbind(
+  titles_df, 
+  select(dates_data, -c(`.class`, page_id)),
+  select(links_data, -c(page_id))) %>% 
+  mutate_all(as.character)
 
 # export data
 openxlsx::write.xlsx(dates_data, paste0("output/dates", Sys.Date(), ".xlsx"))
 openxlsx::write.xlsx(links_data, paste0("output/links", Sys.Date(), ".xlsx"))
 openxlsx::write.xlsx(titles_df, paste0("output/titles", Sys.Date(), ".xlsx"))
-openxlsx::write.xlsx(merged_new_data, paste0("output/all_data", Sys.Date(), ".xlsx"))
-write_rds(merged_new_data, paste0("output/all_data", Sys.Date(), ".rds"))
+openxlsx::write.xlsx(merged_data, paste0("output/all_data", Sys.Date(), ".xlsx"))
+write_rds(merged_data, paste0("output/all_data", Sys.Date(), ".rds"))
 
 
